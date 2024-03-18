499c6e10448dfa031c528c663d1ad7be1915b1cc
村山正俊 <masatoshi0058@gmail.com>
2024-03-18 22:01:38 +0900

    AC

---
+++ LCoder_Python/9514.py
@@ -0,0 +1,9 @@
+x = int(input("수를 입력하시오. "))
+y = int(input("수를 입력하시오. "))
+
+print(x, "+", y, "=", x+y)
+print(x, "-", y, "=", x-y)
+print(x, "*", y, "=", x*y)
+print(x, "//", y, "=", x//y)
+print(x, "%", y, "=", x%y)
+print(x, "**", y, "=", x**y)
