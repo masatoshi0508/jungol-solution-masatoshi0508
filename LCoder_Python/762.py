05c77906a07afa59fc105942290b673d49fd214b
村山正俊 <masatoshi0058@gmail.com>
2024-03-22 11:03:34 +0900

    AC

---
+++ LCoder_Python/762.py
@@ -0,0 +1,6 @@
+name1, age1 = input().split()
+name2, age2 = input().split()
+
+total_age = int(age1)+int(age2)
+
+print(name1, "age +", name2, "age =", total_age)
---
+++ LCoder_Python/9524.py
@@ -0,0 +1,3 @@
+a = input()[5:-5]
+
+print(a)
