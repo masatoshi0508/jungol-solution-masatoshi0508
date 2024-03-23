4e6553b5ff1a88f6471688378f4bf50090c6b1c4
村山正俊 <masatoshi0058@gmail.com>
2024-03-23 15:09:41 +0900

    AC

---
+++ LCoder_Python/764.py
@@ -0,0 +1,7 @@
+a1, s1 = input().split()
+a2, s2 = input().split()
+a3, s3 = input().split()
+
+print("{} sounds {}".format(a1, s1))
+print("{} sounds {}".format(a2, s2))
+print("{} sounds {}".format(a3, s3))
---
+++ LCoder_Python/9525.py
@@ -0,0 +1,4 @@
+a, b = input().split()
+
+for _ in range(4):
+    print(a, "like", b)
