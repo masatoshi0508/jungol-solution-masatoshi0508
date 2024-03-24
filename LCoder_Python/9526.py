6222e86822ad666c6b2d8f12a08ba640b5664a71
村山正俊 <masatoshi0058@gmail.com>
2024-03-24 17:38:49 +0900

    AC

---
+++ LCoder_Python/9526.py
@@ -0,0 +1,9 @@
+a, b, c = input().split()
+
+b = int(b)
+c = float(c)
+
+c = "{:.6f}".format(c)
+
+print("{}의 키는 {}cm이며, 몸무게는 {}kg입니다.".format(a, b, c))
+print("{}의 키는 {}cm이며, 몸무게는 {}kg입니다.".format(a, b, c))
