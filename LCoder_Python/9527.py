0aac7eaed691b92d0c689c2e9e83152dfd4f045a
村山正俊 <masatoshi0058@gmail.com>
2024-03-26 22:31:56 +0900

    AC

---
+++ LCoder_Python/9527.py
@@ -0,0 +1,15 @@
+name, kor, eng, mat = input().split()
+
+kor = int(kor)
+eng = int(eng)
+mat = int(mat)
+
+sum = kor+eng+mat
+avg = (kor+eng+mat)/3
+avg = "{:.2f}".format(avg)
+
+print("           name  kor  eng  mat  sum    avg")
+print("    ", name,"",kor,"  ", eng, " ", mat, "", sum, "", avg) 
+print(" ")
+print("           name  kor  eng  mat  sum    avg")
+print("    ", name, "  ", kor, "  ", eng, mat, "  ", sum, "  ", avg) 
--- LCoder_Python/993.py
+++ LCoder_Python/993.py
@@ -4,4 +4,4 @@ avg = float(input())
 
 avg = "{:.6f}".format(avg)
 
-print("I am {}(IDNo. {}). I got {} in my midterm exam".format(name, num, avg))
+print("I am {}(IDNo. {}). I got {} in my midterm exam.".format(name, num, avg))
