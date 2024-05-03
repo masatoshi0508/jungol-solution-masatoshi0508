949f2880a589af641b88593465de9e8bcafbe204
村山正俊 <masatoshi0058@gmail.com>
2024-05-03 10:32:23 +0900

    AC

---
+++ LCoder_Python/848.py
@@ -0,0 +1,6 @@
+rows = 4
+
+for i in range(2, 2+rows):
+    for j in range(i, i+5):
+        print(j, end=" ")
+    print()
---
+++ LCoder_Python/9565.py
@@ -0,0 +1,9 @@
+for i in range(1, 6):
+    i = str(i)
+    print((i+" ")*5)
+print()
+
+for j in range(1, 6):
+    for k in range(1, 6):
+        print(k, end=' ')
+    print()
