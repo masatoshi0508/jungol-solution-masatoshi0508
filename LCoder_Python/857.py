d0fecf1f9a40be2c03a1c4e17d45fd48db5167bd
村山正俊 <masatoshi0058@gmail.com>
2024-05-11 20:16:31 +0900

    AC

---
+++ LCoder_Python/857.py
@@ -0,0 +1,14 @@
+x, y = map(int, input().split())
+matrix = []
+
+for i in range(1, x+1):
+    x = []
+    for j in range(1, y+1):
+        element = i * j
+        x.append(element)
+    matrix.append(x)
+
+for x in matrix:
+    for element in x:
+        print(element, end=" ")
+    print()
