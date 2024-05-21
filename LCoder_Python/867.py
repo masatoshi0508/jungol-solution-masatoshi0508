39d8aee844ec6406f06114fce1036a0428315c3f
村山正俊 <masatoshi0058@gmail.com>
2024-05-21 21:25:06 +0900

    AC

--- LCoder_Python/867.py
+++ LCoder_Python/867.py
@@ -1,7 +1,7 @@
 n = int(input())
 current_char = ord("A")  # 'A'のASCIIコードを取得
 
-for i in range(1, n+1):  # 1からnumまでの数字でループ
+for i in range(n, 0, -1):  # 1からnumまでの数字でループ
     line = ""            # 各行の文字列を初期化
     for j in range(i):   # i回ループして文字を追加
         line = line + chr(current_char)   # ASCIIコードを文字に変換して追加
